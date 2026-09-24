aeca453526bc862d91611d33eb2570f0467041f0
Pipenko Mariia <hiww226501@gmail.com>
2026-09-24 17:45:40 +0300

    commit

--- tema26/tema26.cpp
+++ tema26/tema26.cpp
@@ -1,6 +1,19 @@
-#include <iostream>
+#include "CarNumber.h"
+int CarNumber::counter = 9999;
+char CarNumber::seria1 = 'A';
+char CarNumber::seria2 = 'Z';
+string CarNumber::region = "AX";
 
 int main()
 {
-    
+    CarNumber::showStatic();
+    CarNumber a1;
+    CarNumber a2;
+    CarNumber a3;
+
+    cout << a1.getNumber()<<endl;
+    cout << a2.getNumber()<<endl;
+    cout << a3.getNumber()<<endl;
+    a1.showStatic();
+    a3.showStatic();
 }
